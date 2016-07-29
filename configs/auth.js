2638736d6cfc05da0d20b812df1384c93b87cffa
Eliseu Benedito Codinhoto <zeucxb@gmail.com>
2016-07-29 03:19:37 -0300

    Remove JWT

--- configs/auth.js
+++
@@ -1,7 +0,0 @@
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-
-module.exports = {
-	jwtSecret: 'socorrammesubinoonibusemmarrocos',
-	jwtSession: {session: false},
-	jwtFromRequest: ExtractJwt.fromAuthHeader()
-};
